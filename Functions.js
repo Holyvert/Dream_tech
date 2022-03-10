d60ae6a4ad5fc0e9ee23115ddcf6538f22e3d5c9
Aly Hussein <83790018+Aly-Hussein@users.noreply.github.com>
2022-03-09 20:05:39 -0500

    made cart responsive and product pages

--- Functions.js
+++ Functions.js
@@ -13,6 +13,7 @@ function ready()
     var productPagePrice = parseFloat(document.getElementById('price').innerText.replace('$',''))
     var productPageQuantity = document.getElementById("amount").value
     document.getElementById('price1').innerText = Math.round(productPageQuantity * productPagePrice * 100)/100 + " $"
+    deleteButton()
 }   
 
 function updateQuantitiy()
@@ -50,3 +51,65 @@ function setQuantity(upordown){
         updateQuantitiy();}
 }
 
+function deleteButton()
+{
+    var removeCartItemButtons = document.getElementsByName("delete")
+    console.log(removeCartItemButtons)
+    for(var i = 0;i<removeCartItemButtons.length;i++)
+    {
+        var button = removeCartItemButtons[i]
+        button.addEventListener('click',function(event){
+            var buttonClicked = event.target
+            buttonClicked.parentElement.parentElement.parentElement.remove()
+            updateCartTotal()
+        })
+        
+    }
+
+}
+function setCartQuantity(element,upordown)
+{
+    var amount = element.parentElement.children[1].value
+    if (amount > 1) 
+        {
+        if (upordown == 'up')
+        {
+            ++element.parentElement.children[1].value
+            updateCartTotal()
+            
+        }
+        else if (upordown == 'down')
+        {
+            --element.parentElement.children[1].value
+            updateCartTotal()
+        }
+    }
+    else if (amount == 1) 
+    {
+        if (upordown == 'up')
+        {
+            ++element.parentElement.children[1].value
+            updateCartTotal()
+        }
+    }
+    else
+        {
+            element.parentElement.children[1].value=1
+            updateCartTotal()
+        }
+    
+}
+function updateCartTotal()
+{
+   cartQuantitiy = document.getElementsByName('quantity')
+   cartPrice = document.getElementsByName('cartPrice')
+   
+    var total = 0
+   for(var i = 0;i<cartQuantitiy.length;i++)
+   {
+    
+      total = total + cartQuantitiy[i].value * parseFloat(cartPrice[i].innerText.replace('$',''))
+    
+   }
+   document.getElementsByName('priceB4Taxes')[0].innerText = Math.round( total* 100)/100 + " $"
+}
