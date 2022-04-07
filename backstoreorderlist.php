eb3658af072a2de760248fb3f822b90372259ba9
amczuboka <94947740+amczuboka@users.noreply.github.com>
2022-04-06 22:15:07 -0400

    just trying to add session_start and added a new file for php

---
+++ backstoreorderlist.php
@@ -0,0 +1,18 @@
+<?php
+if(!isset($_COOKIE['User'])){
+    setcookie('User', 'id', time()+3600);
+    session_start();
+}
+?>
+<?php
+//$get_cart_items_name = $_GET("CartProd"[0]);
+$get_cart_items_quantity = (array)$_GET["quantity"];
+//$get_cart_items_price = $_GET["cartPrice"];
+?>
+    
+    <?php
+     foreach ($get_cart_items_quantity as $key => $value) {
+        echo "$key: $value\n";
+}
+    ?>
+
