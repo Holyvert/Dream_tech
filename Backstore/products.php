a3948aefad2e84085f8d6c5c3614c80c08154bb3
Holyvert <96556635+Holyvert@users.noreply.github.com>
2022-04-10 18:26:43 -0400

    a few format changes

--- Backstore/products.php
+++ Backstore/products.php
@@ -15,9 +15,44 @@
         'Quantity_inStock'=> $_POST['Quantity'] 
         );
 
-    $fp=fopen('Products.json', 'a');
-    fwrite($fp,json_encode($datae));
-    fclose($fp);
+  
+       $filename='Products.json';
+    
+    // read the file if present
+$handle = @fopen($filename, 'r+');
+
+// create the file if needed
+if ($handle === null)
+{
+    $handle = fopen($filename, 'w+');
+}
+
+if ($handle)
+{
+    // seek to the end
+    fseek($handle, 0, SEEK_END);
+
+    // are we at the end of is the file empty
+    if (ftell($handle) > 0)
+    {
+        // move back a byte
+        fseek($handle, -1, SEEK_END);
+
+        // add the trailing comma
+        fwrite($handle, ',', 1);
+
+        // add the new json string
+        fwrite($handle, json_encode($datae) . ']');
+    }
+    else
+    {
+        // write the first event inside an array
+        fwrite($handle, json_encode($datae));
+    }
+
+        // close the handle on the file
+        fclose($handle);
+}
 
  }
 ?>
