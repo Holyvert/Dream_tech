d5fca6c2fc06c0c3db02cdfb76eda007bacff603
Holyvert <96556635+Holyvert@users.noreply.github.com>
2022-04-17 18:32:14 -0400

    yep

--- header2.php
+++ header2.php
@@ -2,14 +2,22 @@
         <div class="ShopSign">
           <p class="TShop"><a href="../Website.php"><img class="Logo" src="../Images/Food_box_logo_white.png" alt="SIUU" height="50" /> </a></p>
       </div>
-      <form class="sign">
-      <input
-        class="SearchBar"
-        type="text"
-        placeholder="Search for Products"
-        id="search"
-      /><ul class="SearchBar" id="result"></ul>
-        </form>
+     
+      
+      
+      <?php
+          if (isset($_COOKIE["user_email"])) {
+            echo"<form class=\"sign\">";
+            echo"<button class=\"dropbutton\">";
+            echo"Welcome ".$_COOKIE["user_email"];
+
+            echo "  </button>";
+            echo "  </form>";
+          }
+          ?>
+    
+      
+      
       
         <form class="SignUp">
           <table class="Table">
@@ -17,10 +25,23 @@
               <!-- rows of the tables -->
               <th><a href="../Website.php">Home </a></th>
               <!-- column -->
-              <th>Services</th>
+              <th> Services</th>
               <th>Shop</th>
             <th>
-            <a href="../Login.html">SignIn</a>
+
+          
+         
+
+
+            <?php
+          if (isset($_COOKIE["user_email"])) {
+            echo " <a href=\"../LoginAndRegistration/loginUser.php\"> SignOut </a>";
+            
+            } else{
+              echo " <a href=\"../LoginAndRegistration/loginUser.php\" >SignIn</a>";
+            }
+            ?>
+ 
             </th>
             </tr>
           </table>
