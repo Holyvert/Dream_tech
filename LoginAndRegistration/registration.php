33bc43ac5f4ffe6dc0dcd8fe54967002d47cf311
AhmedSabsoob <ahmed.sabsoobdf@gmail.com>
2022-04-17 17:55:30 -0400

    Fixed indentation + 3aloosh redirected the users data to users.json ❤️

--- LoginAndRegistration/registration.php
+++ LoginAndRegistration/registration.php
@@ -7,29 +7,28 @@
 <body>
   <?php
 
-include "../Backstore/usersFunctions.php";
+  include "../Backstore/usersFunctions.php";
 
-$users = getUsers2();
+  $users = getUsers2();
 
-error_reporting(E_ALL ^ E_WARNING);
+  error_reporting(E_ALL ^ E_WARNING);
 
 
   if (isset($_POST["submit"])) { // the POST form has been submitted.
 
-    
-   
+
+
 
 
     foreach ($users as $i => $user) {
-      if (strcmp($user['Email'], $_POST['email'])  == 0) 
-      {
-         echo "Email already exists";
-         
-         echo ("<script> location.href = 'loginUser.php'</script>");
-         break;
+      if (strcmp($user['Email'], $_POST['email'])  == 0) {
+        echo "Email already exists";
+
+        echo ("<script> location.href = 'loginUser.php'</script>");
+        break;
       }
     }
-    createUser2($_POST) ;
+    createUser2($_POST);
     echo ("<script> location.href = 'loginUser.php'</script>");
   }
   ?>
